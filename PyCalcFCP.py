ccd3102311593f2e4fdf6da6f4bd4ca5050794fe
Flames Elite <rinagunnerx@gmail.com>
2023-04-26 05:54:24 -0700

    Create PyCalcFCP.py

---
+++ PyCalcFCP.py
@@ -0,0 +1,57 @@
+def calculator():
+  """A calculator that adds, subtracts, multiplies, and divides two numbers."""
+
+  # Ask user for first number:
+  num1 = float(input("Enter the first number: "))
+
+  # Ask user for second number:
+  num2 = float(input("Enter the second number: "))
+
+  # Addition of two numbers
+  sum = num1 + num2
+  print(f"{num1} + {num2} = {sum}")
+
+  # Subtraction of two numbers
+  sub = num1 - num2
+  print(f"{num1} - {num2} = {sub}")
+
+  # Multiplication of two numbers
+  mul = num1 * num2
+  print(f"{num1} * {num2} = {mul}")
+
+  # Division of two numbers
+  div = num1 / num2
+  print(f"{num1} / {num2} = {div}")
+
+
+if __name__ == "__main__":
+  calculator()
+def calculator():
+  """A calculator that adds, subtracts, multiplies, and divides two numbers."""
+
+  # Ask user for first number:
+  num1 = float(input("Enter the first number: "))
+
+  # Ask user for second number:
+  num2 = float(input("Enter the second number: "))
+
+  # Addition of two numbers
+  sum = num1 + num2
+  print(f"{num1} + {num2} = {sum}")
+
+  # Subtraction of two numbers
+  sub = num1 - num2
+  print(f"{num1} - {num2} = {sub}")
+
+  # Multiplication of two numbers
+  mul = num1 * num2
+  print(f"{num1} * {num2} = {mul}")
+
+  # Division of two numbers
+  div = num1 / num2
+  print(f"{num1} / {num2} = {div}")
+
+
+if __name__ == "__main__":
+  calculator()
+## [FCP LABS 20XX]   - [C]
